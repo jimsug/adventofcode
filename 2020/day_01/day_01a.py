ca0d3ffdd5082c5b5670386442daf159ddda2fcd
jimsug <jimsug@pm.me>
2020-12-01 23:46:32 +1100

    fix laziness in strip()[:-1]

--- 2020/day_01/day_01a.py
+++ 2020/day_01/day_01a.py
@@ -1,7 +1,7 @@
 #!/usr/bin python
 from itertools import combinations
 with open("2020/day_01/input_01a.txt") as f:
-    for i in combinations([int(x) for x in f.read().split('\n')[:-1]], 2):
+    for i in combinations([int(x) for x in f.read().strip().split('\n')], 2):
         if sum(i) == 2020:
             print(
                 "{0} + {1} = {2}; {0} x {1} = {3}".format(i[0], i[1], i[0] + i[1], i[0] * i[1]))
--- 2020/day_01/day_01b.py
+++ 2020/day_01/day_01b.py
@@ -1,7 +1,7 @@
 #!/usr/bin python
 from itertools import combinations
 with open("2020/day_01/input_01a.txt") as f:
-    for i in combinations([int(x) for x in f.read().split('\n')[:-1]], 3):
+    for i in combinations([int(x) for x in f.read().strip().split('\n')], 3):
         if sum(i) == 2020:
             print("{0} + {1} + {2} = {3};  {0} x {1}  x {2} = {4}".format(
                 i[0], i[1], i[2], sum(i),  i[0]*i[1]*i[2]))
