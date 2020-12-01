88c0d51d999f80d6a400f1b8c3ddb7ce64026d61
jimsug <jimsug@pm.me>
2020-12-01 22:32:57 +1100

    pep8

--- 2020/day_01/day_01a.py
+++ 2020/day_01/day_01a.py
@@ -8,4 +8,5 @@ input = [int(x) for x in f.read().split('\n')[:-1]]
 
 for i in combinations(input, 2):
     if i[0] + i[1] == 2020:
-        print("{0} + {1} = {2}; {0} x {1} = {3}".format(i[0], i[1], i[0] + i[1], i[0] * i[1] ))
+        print(
+            "{0} + {1} = {2}; {0} x {1} = {3}".format(i[0], i[1], i[0] + i[1], i[0] * i[1]))
--- 2020/day_01/day_01b.py
+++ 2020/day_01/day_01b.py
@@ -8,4 +8,5 @@ input = [int(x) for x in f.read().split('\n')[:-1]]
 
 for i in combinations(input, 3):
     if sum(i) == 2020:
-        print("{0} + {1} + {2} = {3};  {0} x {1}  x {2} = {4}".format(i[0], i[1], i[2], sum(i),  i[0]*i[1]*i[2]))
+        print("{0} + {1} + {2} = {3};  {0} x {1}  x {2} = {4}".format(
+            i[0], i[1], i[2], sum(i),  i[0]*i[1]*i[2]))
