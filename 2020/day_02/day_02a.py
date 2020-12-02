99f86534437f21d38e875f2b8552eb44be712a75
jimsug <jimsug@pm.me>
2020-12-02 23:16:52 +1100

    typos

--- 2020/day_02/day_02a.py
+++ 2020/day_02/day_02a.py
@@ -4,7 +4,7 @@ with open("2020/day_02/input_02a.txt") as f:
     i = f.read().strip().split("\n")
     o = 0
     for x in i:
-        y = re.split('[ \-:]', x)
+        y = re.split(r'[ \-:]', x)
         z = len(re.findall(y[2], y[4]))
         if (z >= int(y[0])) and (z <= int(y[1])):
             o += 1
--- 2020/day_02/day_02b.py
+++ 2020/day_02/day_02b.py
@@ -4,7 +4,7 @@ with open("2020/day_02/input_02a.txt") as f:
     i = f.read().strip().split("\n")
     o = 0
     for x in i:
-        y = re.split('[ \-:]', x)
+        y = re.split(r'[ \-:]', x)
         if (y[2] == y[4][int(y[0])-1]) ^ (y[2] == y[4][int(y[1])-1]):
             o += 1
 
