5b51ca5982ec15c82e40a052451cb8673445b4e3
jimsug <js@sugrono.com>
2020-12-10 21:47:19 +1100

    fluff

--- 2020/day_10/day_10a.py
+++ 2020/day_10/day_10a.py
@@ -1,14 +1,9 @@
 with open("2020/day_10/input.txt") as f:
     input = [int(i) for i in f.read().strip().split("\n")]
 
+input.append(0)
+input.append(max(input)+3)
 input.sort()
 
-joltage = 0
-i = 0
-
 diffArray = [input[i]-input[i-1] for i in range(1, len(input))]
-diffArray.append(3)
-diffArray.append(min(input)-0)
-
-diffArray.sort()
 print(diffArray.count(1)*diffArray.count(3))
