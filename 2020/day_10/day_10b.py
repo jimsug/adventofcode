836d42ecf564d1222320912adca78d281a6f7317
jimsug <js@sugrono.com>
2020-12-10 19:06:29 +1100

    cruft

--- 2020/day_10/day_10b.py
+++ 2020/day_10/day_10b.py
@@ -6,5 +6,5 @@ steps = [0] * (input[-1]+1)
 
 steps[0] = 1
 for n in input:
-    steps[n] = steps[n-3] + steps[n-2] + steps[n-1]
+    steps[n] = steps[n-1] + steps[n-2] + steps[n-3]
     print(steps[n], steps)
