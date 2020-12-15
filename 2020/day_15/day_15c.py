cb8465a1d518d38f570d473697d7f7d7f3ed0826
jimsug <js@sugrono.com>
2020-12-15 22:02:17 +1100

    remove

--- 2020/day_15/day_15c.py
+++
@@ -1,5 +0,0 @@
-for part in [2020,30000000]:
-    nums, one = { e:i+1 for i,e in enumerate([19,0,5,1,10,13]) }, 19
-    for turn in range(7, part):
-        nums[one], one = turn, 0 if one not in nums else turn-nums[one]
-    print(one)
