d120bf267d9e1a1b79d8992201d9340ca0163623
jimsug <js@sugrono.com>
2020-12-15 22:01:38 +1100

    .

--- 2020/day_15/day_15c.py
+++ 2020/day_15/day_15c.py
@@ -1,19 +1,5 @@
-#!/usr/bin/python3
-
-input = [19,0,5,1,10,13]
-targets = [2020, 3*(10**7)]
-for a in range(len(targets)):
-
-    seen = [-1]*targets[a]
-    mem = [-1]*targets[a]
-    for i in range(len(input)):
-        seen[input[i]] = i
-        mem[i] = input[i]
-
-    i = len(input) - 1
-
-    for j in range(i, targets[a]-1):
-        mem[j+1] = j - seen[mem[j]] if seen[mem[j]] >= 0 else 0
-        seen[mem[j]] = j
-
-    print("Part {} ({}th item):".format(a+1, len(mem)), mem[-1])
+for part in [2020,30000000]:
+    nums, one = { e:i+1 for i,e in enumerate([19,0,5,1,10,13]) }, 19
+    for turn in range(7, part):
+        nums[one], one = turn, 0 if one not in nums else turn-nums[one]
+    print(one)
