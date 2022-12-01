5bb4895cc18ac224600d5ab930b5fb472a320603
jimsug <jimsug@pm.me>
2022-12-01 22:40:54 +1100

    fix day 1

--- 2022/01.py
+++ 2022/01.py
@@ -1,8 +1,7 @@
 from aocd.models import Puzzle
 p = Puzzle(year=2022, day=1).input_data
 
-for i in p.split("\n\n"):
-    totals = [sum([int(n) for n in i.split("\n")]) for i in p.split("\n\n")]
+totals = [sum([int(n) for n in i.split("\n")]) for i in p.split("\n\n")]
 
 totals.sort()
 
